1635b6c86c8ea2e9f343941ad421397110a73ef3
Rubxn <rubenbm198@gmail.com>
2023-03-20 16:27:39 +0100

    reducer

--- src/context/myList.jsx
+++ src/context/myList.jsx
@@ -1,59 +1,12 @@
-/* eslint-disable default-case */
 import { createContext, useReducer } from "react"
+import { myListActionsReducer, initialState } from "../reducer/useListReducer"
+
 export const MyListContext = createContext()
 
-export const initialState = JSON.parse(window.localStorage.getItem('list')) || []
-
-function updateLocalStorage(list) {
-    window.localStorage.setItem('list', JSON.stringify(list))
-}
-
-export const reducer = (state, action) => {
-    const {type: actionType, payload: actionPayload } = action
-
-    switch(actionType) {
-        case 'ADD_RATING': {
-            
-            const {movie, rating, description} = actionPayload
-
-            const filmInListIndex = state.findIndex(item => item.id === movie.id)
-
-            if(filmInListIndex >= 0) {
-
-                const newList = structuredClone(state)
-                newList[filmInListIndex].rating = rating 
-                newList[filmInListIndex].description = description 
-                updateLocalStorage(newList)
-                return newList
-
-            } else {
-
-                const newList = [
-                    ...state,
-                    {
-                        ...movie, 
-                        rating: rating,
-                        description: description
-                    }
-                ]
-                
-                updateLocalStorage(newList)
-                return newList
-            }
-        }
-
-        case 'CLEAR_LIST': {
-            updateLocalStorage([])
-            return []
-        }
-    }
-
-    return state
-}
 
 export function MyListProvider({children}) {
     
-    const [state, dispatch ] = useReducer(reducer, initialState)
+    const [state, dispatch ] = useReducer(myListActionsReducer, initialState)
 
     const addRating = (movie, rating, description) => dispatch({
         type: 'ADD_RATING',
