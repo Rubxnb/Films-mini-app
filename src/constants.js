125fc37939474ee36fe8640fb04bad12c99627a1
Rubxn <rubenbm198@gmail.com>
2023-04-01 16:42:15 +0200

    Updates

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-
-export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/original/'
-
-export const EVENTS = {
-    PUSHSTATE: 'pushstate',
-    POPSTATE: 'popstate',
-}
--- src/pages/Error.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export function Error() {
-  return (
-    <h1>404 ERROR</h1>
-  )
-}
